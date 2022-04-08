a48c758ab8299360eef560f2702739b42cb73b6f
Sean Keane <seankeane93@gmail.com>
2022-04-08 13:41:24 -0700

    add branching logic between questions

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,19 @@ $document.ready(function() {
 
     function next_question() {
 
+        const questionOne = (document).quiz.question1.value;
+        console.log('In next question ' + questionOne);
+        if (questionOne === 'Book' || questionOne === 'Exercise') {
+            $('#question1').hide();
+            $('#question2').show();
+        } else if (questionOne === 'Beach' || questionOne === 'Sports') {
+            $('#question1').hide();
+            $('#question3').show();
+        }
+
+        const questionTwo = (document).quiz.question2.value;
+        console.log('In the next question ' + questionTwo);
+
 
     }
 });
